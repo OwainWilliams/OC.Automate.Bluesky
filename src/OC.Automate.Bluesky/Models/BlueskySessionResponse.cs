2db2670276a2d8c5a635db0dcd55bdeb28848c46
Owain <ow@initials.co.uk>
2026-06-18 12:01:37 +0100

    [FEAT] Add Manifest to auto version package

---
+++ src/OC.Automate.Bluesky/Models/BlueskySessionResponse.cs
@@ -0,0 +1,8 @@
+namespace OC.Automate.Bluesky;
+
+public sealed class BlueskySessionResponse
+{
+    public string? AccessJwt { get; set; }
+    public string? Did { get; set; }
+    public string? Handle { get; set; }
+}
